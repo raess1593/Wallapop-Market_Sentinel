7a85564ab63bc092d2496ae0e106f7e971d99fec
raess1593 <raess1593@gmail.com>
2026-02-01 17:59:09 +0100

    feat: implement the main data scraping and processing flow
    The call to get_page_articles for raw data collection is integrated.
    The DataProcessor class is instantiated with the collected data.
    The clean() method is called to execute the cleaning logic (prices, names, nulls).
    The final resulting DataFrame is displayed in the console.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from scraper import WallapopScraper
+from processor import DataProcessor
 import argparse
 
 if __name__ == "__main__":
+    # Receive and parse arguments
     parser = argparse.ArgumentParser(description="Wallapop Market Sentinel Scraper")
     parser.add_argument("--search", type=str, default="nike", help="Key word to search ex: nike")
     parser.add_argument("--limit", type=int, default=10, help="Number of articles to extract ex: 200")
@@ -9,12 +11,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
     scraper = WallapopScraper()
 
+    # Search a specific url according to the parameters
     base_url = "https://es.wallapop.com/search?keywords="
     params = "&order_by=most_relevance"
     search_query = args.search.replace(" ", "%20")
     url = f"{base_url}{search_query}{params}"
     scraper.fetch_page(url)
     
+    # Get data
     scraper.get_page_articles(args.limit)
-    #scraper.get_dataframe()
+    raw_data = scraper.data
+
+    # Convert data to a clean dataframe
+    df_object = DataProcessor(raw_data)
+    df = df_object.clean()
+    print(df)
+
     scraper.close()
